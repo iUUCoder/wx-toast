36dff92b8494c29b3533053f03b453c98f95a62f
UUCoder <uucoder@yeah.net>
2019-11-24 22:19:10 +0800

    集成ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "extends": "eslint-config-standard",
+    "globals": {
+        "wx": "readonly",
+    },
+    "rules": {
+    }
+};
